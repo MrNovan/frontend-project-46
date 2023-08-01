a251c24bff40597809fded51d1e520dae734a456
MrNovan <toni.justdoit@bk.ru>
2023-08-01 10:46:54 +0300

    add format type

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,9 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-v, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .action((filepath1, filepath2) => {
     const options = program.opts().format;
     const result = gendiff(filepath1, filepath2, options);
