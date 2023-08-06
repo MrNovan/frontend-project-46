8a5599fcd89a576187eaa68e480e50726edf6943
MrNovan <toni.justdoit@bk.ru>
2023-08-06 15:58:17 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import process from 'process';
-import getDiff from '../src/index.js';
+import getDiff from '../src/getdiff.js';
 
 const program = new Command();
 
